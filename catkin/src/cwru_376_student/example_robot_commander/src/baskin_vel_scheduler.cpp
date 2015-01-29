8defa21e86ed2563f4f9233db9485c13a00b49fe
Buck Baskin <mobile.wbaskin@gmail.com>
2015-01-29 15:58:59 -0500

    Code Cleanup

--- catkin/src/cwru_376_student/example_robot_commander/src/baskin_vel_scheduler.cpp
+++ catkin/src/cwru_376_student/example_robot_commander/src/baskin_vel_scheduler.cpp
@@ -22,7 +22,7 @@ qw = cos(angle/2)
 therefore, theta = 2*atan2(qz,qw)
 */
 
-//rosrun stdr_robot robot_handler replace /robot0 27 21 3.9
+//to reset robot in stdr: rosrun stdr_robot robot_handler replace /robot0 27 21 3.9
 
 
 #include <ros/ros.h>
@@ -31,57 +31,68 @@ therefore, theta = 2*atan2(qz,qw)
 #include <nav_msgs/Odometry.h>
 #include <math.h>
 #include <vector>
-// set some dynamic limits...
-const double v_max = 5.0; //1m/sec is a slow walk
+/* 
+	DYNAMIC LIMITS 
+ */
+const double v_max = 1.0; //1m/sec is a slow walk
 const double v_min = 0.1; // if command velocity too low, robot won't move
 const double a_max = 0.1; //1m/sec^2 is 0.1 g's
-//const double a_max_decel = 0.1; // TEST
+
 const double omega_max = 1.0; //1 rad/sec-> about 6 seconds to rotate 1 full rev
 const double alpha_max = 0.5; //0.5 rad/sec^2-> takes 2 sec to get from rest to full omega
-const double DT = 0.050; // choose an update rate of 20Hz; go faster with actual hardware
+
+//proportional control adjustment parameters
+double linear_gain = 0.25;
+double angular_gain = 0.5;
+
+const double DT = 0.050; // update rate of 20Hz; faster with actual hardware
 std::vector< double > path;
 int segment_index = 0;
-// globals for communication w/ callbacks:
-double odom_vel_ = 0.0; // measured/published system speed
-double odom_omega_ = 0.0; // measured/published system yaw rate (spin)
+
+double odom_vel_ = 0.0; //robot speed
+double odom_omega_ = 0.0; // robot spin
 double odom_x_ = 0.0;
 double odom_y_ = 0.0;
 double odom_phi_ = 0.0;
 double dt_odom_ = 0.0;
 ros::Time t_last_callback_;
 double dt_callback_=0.0;
-// receive the pose and velocity estimates from the simulator (or the physical robot)
-// copy the relevant values to global variables, for use by "main"
-// Note: stdr updates odom only at 10Hz; Jinx is 50Hz (?)
+
 void odomCallback(const nav_msgs::Odometry& odom_rcvd) {
-	//here's a trick to compute the delta-time between successive callbacks:
+	// compute the delta-time between successive callbacks:
 	dt_callback_ = (ros::Time::now() - t_last_callback_).toSec();
-	t_last_callback_ = ros::Time::now(); // let's remember the current time, and use it next iteration
-	if (dt_callback_ > 0.15) { // on start-up, and with occasional hiccups, this delta-time can be unexpectedly large
-		dt_callback_ = 0.1; // can choose to clamp a max value on this, if dt_callback is used for computations elsewhere
-		ROS_WARN("large dt; dt = %lf", dt_callback_); // let's complain whenever this happens
+	t_last_callback_ = ros::Time::now();
+
+	if (dt_callback_ > 0.15) { // avoid eccessively large dt
+		dt_callback_ = 0.1; 
+		ROS_WARN("large dt; dt = %lf", dt_callback_);
 	}
-	// copy some of the components of the received message into global vars, for use by "main()"
-	// we care about speed and spin, as well as position estimates x,y and heading
+	// move current state to global vars for main
 	odom_vel_ = odom_rcvd.twist.twist.linear.x;
 	odom_omega_ = odom_rcvd.twist.twist.angular.z;
 	odom_x_ = odom_rcvd.pose.pose.position.x;
 	odom_y_ = odom_rcvd.pose.pose.position.y;
-	//odom publishes orientation as a quaternion. Convert this to a simple heading
-	// see notes above for conversion for simple planar motion
+	//Convert quaternion to heading in 2D
 	double quat_z = odom_rcvd.pose.pose.orientation.z;
 	double quat_w = odom_rcvd.pose.pose.orientation.w;
-	odom_phi_ = 2.0*atan2(quat_z, quat_w); // cheap conversion from quaternion to heading for planar motion
-	// the output below could get annoying; may comment this out, but useful initially for debugging
-	//ROS_INFO("odom CB: x = %f, y= %f, phi = %f, v = %f, omega = %f", odom_x_, odom_y_, odom_phi_, odom_vel_, odom_omega_);
+	odom_phi_ = 2.0*atan2(quat_z, quat_w);
 }
+
+
+/*
+	MAIN
+*/
+
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "vel_scheduler_wcb38");
 	ros::NodeHandle nh;
 	ros::Publisher vel_cmd_pub = nh.advertise<geometry_msgs::Twist>("robot0/cmd_vel", 1);
 	ros::Subscriber odom_sub = nh.subscribe("/robot0/odom", 1, odomCallback);
 	ros::Rate rtimer(1 / DT);
-	path.resize(6,0.0); // index % 2 == 0 --> straight_dist in meters. index%2 == 1 --> turn_angle in radians.
+	path.resize(6,0.0); 
+	/*
+		index % 2 == 0 --> straight_dist in meters. index%2 == 1 --> turn_angle in radians.
+	 */
 	path[0] =  4.666;
 	path[1] = -1.54;
 	path[2] =  12.25;
@@ -89,39 +100,41 @@ int main(int argc, char **argv) {
 	path[4] =  8.0;
 	path[5] =  0.0; //done
 
-
+	/*
+		Initialize processing variables
+	*/
+	//segment information
 	double segment_length = 0.0;
-	double segment_length_done = 0.0; // need to compute actual distance travelled within the current segment
-
-	double start_x = 0.0; // fill these in with actual values once odom message is received
-	double start_y = 0.0; // subsequent segment start coordinates should be specified relative to end of previous segment
+	double segment_length_done = 0.0;
+	double start_x = 0.0;
+	double start_y = 0.0;
 	double start_phi = 0.0;
 
-	double scheduled_vel = 0.0; //desired vel, assuming all is per plan
+	//desired output (**note W or V)
+	double scheduled_vel = 0.0;
 	double scheduled_w = 0.0;
 
-	double linear_gain = 0.25;
-	double angular_gain = 0.5;
+	//adjusted output for acceleration limits
+	double new_cmd_vel = 0.0;
+	double new_cmd_omega = 0.0;
 
-	double new_cmd_vel = 0.1; // value of speed to be commanded; update each iteration
-	double new_cmd_omega = 0.0; // update spin rate command as well
-	geometry_msgs::Twist cmd_vel; //create a variable of type "Twist" to publish speed/spin commands
-	cmd_vel.linear.x = 0.0; // initialize these values to zero
+	// message to output
+	geometry_msgs::Twist cmd_vel;
+	cmd_vel.linear.x = 0.0;
 	cmd_vel.linear.y = 0.0;
 	cmd_vel.linear.z = 0.0;
 	cmd_vel.angular.x = 0.0;
 	cmd_vel.angular.y = 0.0;
 	cmd_vel.angular.z = 0.0;
+
 	while(segment_index < path.size() && ros::ok()) {
-		// here is a crude description of one segment of a journey. Will want to generalize this to handle multiple segments
-		// define the desired path length of this segment
-		double segment_length = path[segment_index]; // desired travel distance in meters; anticipate travelling multiple segments
-		//here's a subtlety: might be tempted to measure distance to the goal, instead of distance from the start.
-		// HOWEVER, will NEVER satisfy distance to goal = 0 precisely, but WILL eventually move far enought to satisfy distance travelled condition
-		// let's wait for odom callback to start getting good values...
-		odom_omega_ = 1000000; // absurdly high
+		// Take segment from defined path
+		double segment_length = path[segment_index];
+
+		//wait for new odom data
+		odom_omega_ = 1000000;
 		ROS_INFO("waiting for valid odom callback...");
-		t_last_callback_ = ros::Time::now(); // initialize reference for computed update rate of callback
+		t_last_callback_ = ros::Time::now();
 		while (odom_omega_ > 1000 && ros::ok()) {
 			rtimer.sleep();
 			ros::spinOnce();
@@ -132,37 +145,29 @@ int main(int argc, char **argv) {
 		start_phi = odom_phi_;
 		ROS_INFO("start pose: x %f, y= %f, phi = %f", start_x, start_y, start_phi);
 		/*
-		START CONTROL LOGIC
+			START CONTROL LOGIC
 		*/
-		if (segment_index % 2 == 0) { //linear drive forward (USES NEWMAN CODE)
+		if (segment_index % 2 == 0) { //linear drive forward
 			double max_vel = v_max;
 			double max_accel = a_max;
-			// compute some properties of trapezoidal velocity profile plan:
-			double T_accel = v_max / a_max; //...assumes start from rest
-			double T_decel = v_max / a_max; //(for same decel as accel); assumes brake to full halt
-			double dist_accel = 0.5 * a_max * (T_accel * T_accel); //distance rqd to ramp up to full speed
-			double dist_decel = 0.5 * a_max * (T_decel * T_decel);; //same as ramp-up distance
-			double dist_const_v = segment_length - dist_accel - dist_decel; //if this is <0, never get to full spd
-			double T_const_v = dist_const_v / v_max; //will be <0 if don't get to full speed
-			double T_segment_tot = T_accel + T_decel + T_const_v; // expected duration of this move
-			//dist_decel*= 2.0; // TEST TEST TEST
 			ROS_INFO("forward segment #%d", (segment_index+1));
 			while (ros::ok())
 			{
 				ros::spinOnce();
 				double delta_x = odom_x_ - start_x;
 				double delta_y = odom_y_ - start_y;
+				//calculate distance traveled by distance from start
 				segment_length_done = sqrt(delta_x * delta_x + delta_y * delta_y);
-				//ROS_INFO("dist travelled: %f", segment_length_done);
 				double dist_to_go = segment_length - segment_length_done;
 				ROS_INFO("dist_to_go: %f", dist_to_go);
+
 				//planning speed to go
-				if (dist_to_go<= 0.0) {
+				if (dist_to_go<= 0.0) { // if goal state
 					scheduled_vel=0.0;
 				}
-				else {
+				else { // else request dist_proportional velocity w/in maxes
 					scheduled_vel = std::max(-1.0*max_vel , std::min(max_vel, dist_to_go*linear_gain));
-					if ( std::abs(scheduled_vel) < v_min) {
+					if ( std::abs(scheduled_vel) < v_min) { // if speed is too slow, make it min speed
 						if (scheduled_vel > 0.0) { scheduled_vel = v_min ; }
 						else { scheduled_vel = -v_min ; }
 					}
@@ -180,7 +185,6 @@ int main(int argc, char **argv) {
 				else {
 					new_cmd_vel = scheduled_vel;
 				}
-				//ROS_INFO("cmd vel: %f",new_cmd_vel); // debug output
 
 				//publish message
 				cmd_vel.linear.x = new_cmd_vel;
@@ -190,7 +194,7 @@ int main(int argc, char **argv) {
 					cmd_vel.angular.z = 0.0;
 				}
 				vel_cmd_pub.publish(cmd_vel);
-				if (dist_to_go <= 0.0) break;
+				if (dist_to_go <= 0.0) break; // finish if arrived
 				rtimer.sleep();
 
 			}
@@ -204,30 +208,27 @@ int main(int argc, char **argv) {
 			while (ros::ok())
 			{
 				ros::spinOnce();
+				// calculate distance twisted by distance from start
 				segment_length_done = odom_phi_- start_phi;
-				ROS_INFO("\n\n");
-				ROS_INFO("segment length : %f", segment_length);
-				ROS_INFO("odom_phi_: %f", odom_phi_);
-				ROS_INFO("start_phi: %f", start_phi);
-				ROS_INFO("dist twisted: %f", segment_length_done);
 				double dist_to_go = segment_length - segment_length_done;
 				ROS_INFO("dist to go: %f", dist_to_go);
+				
 				//planning speed to go
-				if (dist_to_go<= 0.0 && segment_length >= 0.0) {
+				if (dist_to_go<= 0.0 && segment_length >= 0.0) { // if goal state 1
 					scheduled_w=0.0;
 				}
-				else if (dist_to_go > 0.0 && segment_length < 0.0 ) {
+				else if (dist_to_go > 0.0 && segment_length < 0.0 ) { // if goal state 2
 					scheduled_w=0.0;
 				}
-				else {
+				else { // else request twist_proportional angular vel w/in maxes
 					scheduled_w = std::max(-1.0*max_w , std::min(max_w, dist_to_go*angular_gain));
 					//if (segment_length < 0.0 ) { scheduled_w = scheduled_w * -1.0; }
-					if ( std::abs(scheduled_w) < .05) {
+					if ( std::abs(scheduled_w) < .05) { //if speed is too slow, make it min speed
 						if (scheduled_w > 0.0) { scheduled_w = 0.05 ; }
 						else { scheduled_w = -0.05 ; }
 					}
 				}
-				ROS_INFO("scheduled_w: %f" , scheduled_w);
+
 				//check against accel limits
 				if (odom_omega_ < scheduled_w - max_alpha*dt_callback_) {
 					double w_test = odom_omega_ + max_alpha*dt_callback_;
@@ -240,7 +241,7 @@ int main(int argc, char **argv) {
 				else {
 					new_cmd_omega = scheduled_w;
 				}
-				//ROS_INFO("cmd vel: %f",new_cmd_omega);
+				
 				//publish message
 				cmd_vel.linear.x = 0.0;
 				cmd_vel.angular.z = new_cmd_omega;
@@ -253,7 +254,8 @@ int main(int argc, char **argv) {
 					cmd_vel.angular.z = 0.0;
 				}
 				vel_cmd_pub.publish(cmd_vel);
-				//leave if done
+				
+				// finish if arrived
 				if (dist_to_go <= 0.0 && segment_length >= 0.0) break;
 				if (dist_to_go >= 0.0 && segment_length <= 0.0) break;
 				rtimer.sleep();
